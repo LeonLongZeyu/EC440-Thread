0509708cf870bdd6d92a7e355dbd84f6ca0a52be
LeonLongZeyu <62172956+LeonLongZeyu@users.noreply.github.com>
2023-03-13 18:50:30 -0400

    Update threads.c

--- threads.c
+++ threads.c
@@ -221,6 +221,7 @@ void pthread_exit(void* value_ptr)
 	}
 }
 
+/*
 int pthread_create(pthread_t* thread, const pthread_attr_t* attr, void* (*start_routine) (void* ), void* arg)
 {
 	attr = NULL; //As specified in the slides
@@ -342,3 +343,4 @@ int pthread_create(pthread_t* thread, const pthread_attr_t* attr, void* (*start_
 
 	return 0;
 }
+*/
